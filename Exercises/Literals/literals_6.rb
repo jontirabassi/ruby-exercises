8ae20a50f4bf70946c5bbd4eb64dc774c8991ce9
Jonathon Tirabassi <jonathontirabassi@gmail.com>
2023-03-10 07:23:02 -0500

    Literals Exercises

---
+++ Exercises/Literals/literals_6.rb
@@ -0,0 +1,4 @@
+puts square3 = 3.5**2
+puts square25 = 25.62 **2
+puts square4 = 4.97**2
+
---
+++ Exercises/Literals/literals_7.rb
@@ -0,0 +1,3 @@
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
+#The error tells us there was a ')' used when it shouldve been a '}'
